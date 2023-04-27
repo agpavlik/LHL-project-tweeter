a4f21200bbe8a76a1316cf2fb9e824221afcca29
agpavlik <agpavlik@gmail.com>
2023-04-27 21:23:35 +0000

    Update function renderTweets - prepend insted of append

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,43 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  // // Test / driver code (temporary). Eventually will get this from the server.
-  // const tweetData = {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": "https://i.imgur.com/73hZDYK.png",
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // }
-
-  // // const data = [
-  // //   {
-  // //     "user": {
-  // //       "name": "Newton",
-  // //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  // //       ,
-  // //       "handle": "@SirIsaac"
-  // //     },
-  // //     "content": {
-  // //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  // //     },
-  // //     "created_at": 1461116232227
-  // //   },
-  // //   {
-  // //     "user": {
-  // //       "name": "Descartes",
-  // //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  // //       "handle": "@rd" },
-  // //     "content": {
-  // //       "text": "Je pense , donc je suis"
-  // //     },
-  // //     "created_at": 1461113959088
-  // //   }
-  // // ]
 
   // Function safely renders insecure text:
   const escape = function (str) {
@@ -55,7 +18,7 @@ $(document).ready(() => {
     $('#tweets-container').empty(); // remove any existing child nodes before adding new ones
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet); // calls createTweetElement for each tweet
-      $('#tweets-container').append($tweet); // takes return value and appends it to the tweets container
+      $('#tweets-container').prepend($tweet); // takes return value and appends it to the tweets container / change for prepend in order to reverse tweet list
     }
   };
 
@@ -87,11 +50,20 @@ $(document).ready(() => {
   const $form = $('.new-tweet-form');
   $form.on('submit', (event) => {
     event.preventDefault();
-    $.ajax({
-      method: 'POST',
-      url: '/tweets',
-      data: $form.serialize()
-    }).then(loadTweets);
+// errors
+    // if ( tweetData.content.text === "" || tweetData.content.text === null) {
+
+
+    // } else if (tweetData.content.text.length > 140) {
+
+
+    // } else {
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: $form.serialize()
+      }).then(loadTweets);
+    }  
   });
 
 // Make a GET request to load the tweet
