c8190b49ad6c9202839f94f230862ed5bc69b264
agpavlik <agpavlik@gmail.com>
2023-04-27 18:49:26 +0000

    Fix problem with text explenation with escape functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,43 +4,43 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
-
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
+  // // Test / driver code (temporary). Eventually will get this from the server.
+  // const tweetData = {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png",
+  //     "handle": "@SirIsaac"
   //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
+  //   "content": {
+  //     "text": "If I have seen further it is by standing on the shoulders of giants"
+  //   },
+  //   "created_at": 1461116232227
+  // }
+
+  // // const data = [
+  // //   {
+  // //     "user": {
+  // //       "name": "Newton",
+  // //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  // //       ,
+  // //       "handle": "@SirIsaac"
+  // //     },
+  // //     "content": {
+  // //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  // //     },
+  // //     "created_at": 1461116232227
+  // //   },
+  // //   {
+  // //     "user": {
+  // //       "name": "Descartes",
+  // //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  // //       "handle": "@rd" },
+  // //     "content": {
+  // //       "text": "Je pense , donc je suis"
+  // //     },
+  // //     "created_at": 1461113959088
+  // //   }
+  // // ]
 
   // Function safely renders insecure text:
   const escape = function (str) {
@@ -70,7 +70,7 @@ $(document).ready(() => {
         <div class="article-header-nickname"><p>${tweetData.user.handle}</p></div>
       </div>
       <div class="article-tweet-text">
-        <p>${CSS.escape(tweetData.content.text)}</p>
+        <p>${escape(tweetData.content.text)}</p>
       </div>
       <div class="article-footer">
         <div class="article-footer-date"><p>${timeago.format(tweetData.created_at)}</p></div>
@@ -103,9 +103,5 @@ $(document).ready(() => {
   };
 
   loadTweets();
-
-
-
-
 
 })
